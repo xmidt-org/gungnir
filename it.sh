165382691910a71a728b14f739cb047ff852d5ac
Jack Murdock <kcajmagic@gmail.com>
2019-01-18 15:34:37 -0800

    add status check to integration tests

--- it.sh
+++ it.sh
@@ -3,9 +3,16 @@
 docker --version
 docker-compose --version
 
+function check() {
+  if [[ $1 -ne 0 ]] ; then
+      exit 1
+  fi
+}
+
 function gungnir-docker {
     GUNGNIR_VERSION=$(make version)
     make docker
+    check $?
 }
 
 function svalinn-docker {
@@ -13,6 +20,8 @@ function svalinn-docker {
     git clone https://github.com/Comcast/codex-svalinn.git 2> /dev/null || true
     cd codex-svalinn
     SVALINN_VERSION=$(make version)
+    make docker
+    check $?
     cd ..
     printf "\n"
 }
@@ -23,6 +32,7 @@ function deploy {
     git clone https://github.com/Comcast/codex.git 2> /dev/null || true
     pushd codex/deploy/docker-compose
     SVALINN_VERSION=$SVALINN_VERSION GUNGNIR_VERSION=$GUNGNIR_VERSION docker stack deploy codex --compose-file docker-compose.yml
+    check $?
     popd
     printf "\n"
 }
@@ -36,5 +46,4 @@ deploy
 go get -d github.com/Comcast/codex/tests/...
 printf "Starting Tests \n\n\n"
 go run github.com/Comcast/codex/tests/runners/travis -feature=codex/tests/features
-
-
+check $?
