c454db5b6f63bda651df37d80f04157a143dba1a
Concourse CI <git_concourse_ci@comcast.com>
2019-08-26 23:03:08 +0000

    [skip ci] bump version from 0.9.1 to 0.9.2

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ import (
 const (
 	applicationName, apiBase = "gungnir", "/api/v1"
 	DEFAULT_KEY_ID           = "current"
-	applicationVersion       = "0.9.1"
+	applicationVersion       = "0.9.2"
 )
 
 type Config struct {
