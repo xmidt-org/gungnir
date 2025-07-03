51733fdc60fdceb751ba6ab9fea3129c15a3a5e6
Maura Fortino <maurafortino@gmail.com>
2025-07-03 10:37:14 -0400

    chore: enabled style check/linter

--- doc.go
+++ doc.go
@@ -6,19 +6,19 @@
 // The purpose of this application is to provide an API
 // that returns event information for a device.
 //
-//     Schemes: https
-//     Host: localhost
-//     BasePath: /api/v1
-//     Version: 0.1.1
+//	Schemes: https
+//	Host: localhost
+//	BasePath: /api/v1
+//	Version: 0.1.1
 //
-//     Produces:
-//     - application/json
+//	Produces:
+//	- application/json
 //
-//     SecurityDefinitions:
-//     bearer_token:
-//         type: http
-//         scheme: bearer
-//         bearerFormat: JWT
+//	SecurityDefinitions:
+//	bearer_token:
+//	    type: http
+//	    scheme: bearer
+//	    bearerFormat: JWT
 //
 // swagger:meta
 package main
